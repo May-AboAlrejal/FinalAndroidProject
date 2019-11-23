3cc48c5156355918a2fdddd9695c05ae3bc3730b
wu000227 <wu000227@algonquinlive.com>
2019-11-22 20:28:51 -0500

    create search layout

--- app/src/main/java/com/mayabo/finalandroidproject/chargestationfinder/ChargeStationFinderActivity.java
+++ app/src/main/java/com/mayabo/finalandroidproject/chargestationfinder/ChargeStationFinderActivity.java
@@ -2,5 +2,18 @@ package com.mayabo.finalandroidproject.chargestationfinder;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Bundle;
+
+import com.mayabo.finalandroidproject.R;
+
 public class ChargeStationFinderActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_charge_station_finder);
+
+        setSupportActionBar(findViewById(R.id.toolbar));
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
 }
